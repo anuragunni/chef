028268c8dfc301f979c8438c4d9517bc7e21d0e2
anjaliunnikrishnan <anj.unnikrishnan2@gmail.com>
2016-06-29 22:01:24 +0530

    Update javaSetup.rb

--- tomcat8_java_recipes/javaSetup.rb
+++ tomcat8_java_recipes/javaSetup.rb
@@ -4,7 +4,7 @@ execute 'install jdk' do
                  if [ -f jdk-8u92-linux-x64.tar.gz ]
                         then break
                  else
-                         wget --no-check-certificate --no-cookies --header 'Coo$
+                         wget --no-check-certificate --no-cookies --header 'Cookie: oraclelicense=accept-securebackup-cookie' http://download.oracle.com/otn-pub/java/jdk/8u92-b14/jdk-8u92-linux-x64.tar.gz
                  fi"
 end
 
