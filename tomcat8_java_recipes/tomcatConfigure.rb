fdac82a1f5fbc4790d84caa55153c22937ec2592
anjaliunnikrishnan <anj.unnikrishnan2@gmail.com>
2016-06-29 22:11:32 +0530

    Update tomcatConfigure.rb

--- tomcat8_java_recipes/tomcatConfigure.rb
+++ tomcat8_java_recipes/tomcatConfigure.rb
@@ -1,3 +1,32 @@
+#!/bin/sh
+execute 'tomcat tar file' do
+        command "
+                 if [ -f 'apache-tomcat-8.0.36.tar.gz' ]
+                        then break
+                 else
+                        wget http://apache.mirrors.tds.net/tomcat/tomcat-8/v8.0$
+                 fi"
+end
+
+execute 'unzip tar file' do
+        command "
+                 if [ -d '/usr/local/tomcat' ]
+                        then break
+                 else
+                        mkdir /usr/local/tomcat
+                 fi
+                 if [ -d apache-tomcat-8.0.36 ]
+                 	then break
+                 else
+                 	tar -xvzf apache-tomcat-8.0.36.tar.gz
+                 fi
+                 if [ -d '/usr/local/tomcat/apache-tomcat-8.0.36' ]
+                 	then break
+                 else
+                 	sudo mv apache-tomcat-8.0.36 /usr/local/tomcat/
+                 fi"
+end
+
 execute 'copying server.xml' do
 	command "sudo cp server.xml /usr/local/tomcat/apache-tomcat-8.0.36/conf/server.xml"
 end
