75142e653c9f7df4607ab0260d88e58946755277
anjaliunnikrishnan <anj.unnikrishnan2@gmail.com>
2016-06-29 21:33:31 +0530

    Update default.rb

--- tomcat8_java_recipes/default.rb
+++ tomcat8_java_recipes/default.rb
@@ -1,3 +1,4 @@
+#!/bin/bash
 execute 'update' do
 	command "sudo apt-get update"
 end
@@ -11,11 +12,21 @@ execute 'install git and ant' do
 end
 
 execute 'tomcat tar file' do
-	command "wget http://apache.mirrors.tds.net/tomcat/tomcat-8/v8.0.36/bin/apache-tomcat-8.0.36.tar.gz"
+        command "
+                 if [ -f 'apache-tomcat-8.0.36.tar.gz' ]
+                        then break
+                 else
+                        wget http://apache.mirrors.tds.net/tomcat/tomcat-8/v8.0$
+                 fi"
 end
 
 execute 'unzip tar file' do
-	command "mkdir /usr/local/tomcat
-		 cd /usr/local/tomcat
-		 tar -xvzf apache-tomcat-8.0.36.tar.gz "
+        command "
+                 if [ -d '/usr/local/tomcat' ]
+                        then break
+                 else
+                        mkdir /usr/local/tomcat
+                 fi
+                 tar -xvzf apache-tomcat-8.0.36.tar.gz
+                 sudo mv apache-tomcat-8.0.36 /usr/local/tomcat/ "
 end
