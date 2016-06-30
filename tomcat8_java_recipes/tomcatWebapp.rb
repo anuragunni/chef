e44a8a4eeebbba7a8551b26fb6414342e227208b
anjaliunnikrishnan <anj.unnikrishnan2@gmail.com>
2016-06-30 16:22:10 +0530

    Update tomcatWebapp.rb

--- tomcat8_java_recipes/tomcatWebapp.rb
+++ tomcat8_java_recipes/tomcatWebapp.rb
@@ -1,13 +1,9 @@
-execute 'create subdirectories to store files' do
-	command "sudo mkdir /usr/local/tomcat/apache-tomcat-8.0.36/webapps/hello
-		 sudo mkdir /usr/local/tomcat/apache-tomcat-8.0.36/webapps/hello/WEB-INF
-		 sudo mkdir /usr/local/tomcat/apache-tomcat-8.0.36/webapps/hello/WEB-INF/classes"
-end
-
 execute 'create webapp' do
-	command "sudo cp index.html /usr/local/tomcat/apache-tomcat-8.0.36/webapps/hello/index.html"
+        command "sudo cp sample.war /usr/local/tomcat/apache-tomcat-8.0.36/webapps/"
 end
 
 execute 'restart tomcat' do
-	command "sudo service tomcat8 restart"
+        command "sudo service tomcat8 restart"
 end
+
+
